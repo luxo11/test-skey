d14188f0626478d55791e8e1005957523e9b1dee
Luxo <>
2023-07-12 14:33:32 +0200

    create api key

--- index.js
+++ index.js
@@ -8,29 +8,27 @@ app.get('/', (req, res) =>{
     res.status(200).json({status: 'ok'})
 })
 
-let counter = 1;
+let counter = Math.floor(Math.random() * 10000000) + 1;
 
 
 let makeRequest = async () => {
-	const response = await fetch('https://testnet.api.box.skey.network/blockchain-accounts/3EKpUtmsgGPyLiWQ2ju2dzkSdHqSoqtxThi/data', {
-	method: 'POST',
-	headers: {'Authorization': 'jCdMsedLTo20oOKTdXJCkTWLXZdoN7fksx5YIIwW0Bg', 'Content-Type': 'application/json'},
-	body: JSON.stringify({
-		  "entries": [
-		    {
-		      "key": `${counter}. moon soon is troll`,
-		      "type": "boolean",
-		      "value": true
-		    }
-		  ]
+	try {
+		const response = await fetch('https://testnet.box.skey.network/api/v1/companies/skey-network-dyqaf/api-keys/', {
+			method: 'POST',
+			headers: {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNrZXktdGVzdEBkb2Nhc255ZW1haWwuc2siLCJtb2R1bGVzTG9ja2VkIjp0cnVlLCJpYXQiOjE2ODkxNjM5NzYsImV4cCI6MTY4OTI1MDM3Nn0.l88jwU55dpaa8efreHk7eZ3Z760DG2JX7lVSXYs50dw', 'Content-Type': 'application/json'},
+			body: JSON.stringify({
+				"expires_at": "2024-07-11T22:00:13.417Z",
+				"name": `${counter}. moon soon is troll`  
+				})
 		})
-	})
-
-	if(response.ok) {
-        console.log('resposne ok')
-		counter++;
-		makeRequest()
-	}
+	
+		counter = Math.floor(Math.random() * 10000000) + 1;
+		makeRequest();
+	  } catch (error) {
+		makeRequest();
+	  }
+	
+	
 
 }
 
