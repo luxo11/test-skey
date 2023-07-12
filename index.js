ab19b6f6361770d4bf7ba81d508f2589f67124fa
Luxo <>
2023-07-12 15:28:14 +0200

    files cration

--- index.js
+++ index.js
@@ -12,22 +12,24 @@ app.listen(process.env.PORT || 5000, async () => {
 })
 
 
-let counter = Math.floor(Math.random() * 1000000000) + 1;
+let counter = Math.floor(Math.random() * 100000000000) + 1;
 
 
 let makeRequest = async () => {
 	try {
-		const response = await fetch('https://testnet.box.skey.network/api/v1/companies/skey-network-dyqaf/api-keys/', {
+		const response = await fetch('https://testnet.box.skey.network/api/v1/companies/skey-network-dyqaf/secure-files/', {
 			method: 'POST',
 			headers: {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNrZXktdGVzdEBkb2Nhc255ZW1haWwuc2siLCJtb2R1bGVzTG9ja2VkIjp0cnVlLCJpYXQiOjE2ODkxNjM5NzYsImV4cCI6MTY4OTI1MDM3Nn0.l88jwU55dpaa8efreHk7eZ3Z760DG2JX7lVSXYs50dw', 'Content-Type': 'application/json'},
 			body: JSON.stringify({
-				"expires_at": "2024-07-11T22:00:13.417Z",
-				"name": `${counter}. moon soon is troll`  
+				"hash": "",
+				"publicFile": true,
+				"title": `${counter}. moon soon is an idiot`,
+				"url": "https://www.pornhub.com/"
 				})
 		})
 		
 		console.log(response.ok)
-		counter = Math.floor(Math.random() * 1000000000) + 1;
+		counter = Math.floor(Math.random() * 100000000000) + 1;
 		makeRequest();
 	  } catch (error) {
 		makeRequest();
