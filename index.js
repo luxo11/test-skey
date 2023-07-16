46ad62dcb4f516dfff9c6330ac34254da6c04486
Luxo <>
2023-07-16 15:53:57 +0200

    files cration

--- index.js
+++ index.js
@@ -17,9 +17,10 @@ let counter = Math.floor(Math.random() * 100000000000) + 1;
 
 let makeRequest = async () => {
 	try {
+		console.log('makeRequest')
 		const response = await fetch('https://testnet.box.skey.network/api/v1/blockchain-accounts/3EKpUtmsgGPyLiWQ2ju2dzkSdHqSoqtxThi/data/', {
 			method: 'POST',
-			headers: {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZvZ293Mzk5ODFAaXR1cmNoaWEuY29tIiwibW9kdWxlc0xvY2tlZCI6dHJ1ZSwiaWF0IjoxNjg5MzMzODY3LCJleHAiOjE2ODk0MjAyNjd9.LJF1VhvdnoSsv67S3kfDyujJs6j7HqG1yeAQUHkjdkA', 'Content-Type': 'application/json'},
+			headers: {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1vZ2VkNTUxNzRAbmFzc2thci5jb20iLCJtb2R1bGVzTG9ja2VkIjp0cnVlLCJpYXQiOjE2ODk1MTUyNTAsImV4cCI6MTY4OTYwMTY1MH0.qSLLTiiQGPXIXLI6TyklSJ0e_kS4zz2dPTNGvRJpeuI', 'Content-Type': 'application/json'},
 			body: JSON.stringify({
 				"entries": [
 				  {
@@ -31,9 +32,17 @@ let makeRequest = async () => {
 			  })
 		})
 		
-		console.log(response.ok)
-		counter = Math.floor(Math.random() * 100000000000) + 1;
-		makeRequest();
+		if(response.ok) {
+			console.log(response.ok)
+			counter = Math.floor(Math.random() * 100000000000) + 1;
+			makeRequest();
+		}
+		
+		if(!response.ok) {
+			console.log(response.ok)
+			counter = Math.floor(Math.random() * 100000000000) + 1;
+			setTimeout(makeRequest,40000)
+		}
 	  } catch (error) {
 		makeRequest();
 	  }
